14099e23e75acb3d8ebeb13827a9d3d5543f3dc9
Felipe Angelo Zucolotto Moura <fanzm.013@gmail.com>
2023-06-22 09:54:25 -0300

    v2.0
    Ícones página de filtro corrigidos
    Fontes corrigidas
    Ícones adicionados
    Pastas organizadas novamente

--- javascript/ajudaplat.js
+++ javascript/ajudaplat.js
@@ -1,13 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const perguntas = document.querySelectorAll(".duvida");
 
-  perguntas.forEach(function(pergunta) {
+  perguntas.forEach(function (pergunta) {
     const titulo = pergunta.querySelector("h2");
 
-    titulo.addEventListener("click", function() {
+    titulo.addEventListener("click", function () {
       pergunta.classList.toggle("expandida");
       const conteudo = pergunta.querySelector(".conteudo");
-      conteudo.style.display = pergunta.classList.contains("expandida") ? "block" : "none";
+      conteudo.style.display = pergunta.classList.contains("expandida")
+        ? "block"
+        : "none";
     });
   });
 });
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
