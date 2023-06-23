830928da0619aca84c0b0d83af97c60453eeb223
Felipe Angelo Zucolotto Moura <fanzm.013@gmail.com>
2023-06-23 10:02:34 -0300

    v3.0
    Padrão de Header, Barra de Navegação e Footer adicionados nas páginas devidas.
    Ajuste de ícones.
    Link para as páginas no HTML utilizando a tag <a>.

--- javascript/ajudaplat.js
+++ javascript/ajudaplat.js
@@ -14,11 +14,23 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+function removerCabecalho() {
+  var cabecalho = document.querySelector("#cabecalho");
+  if (cabecalho == null) {
+    return false;
   } else {
-    x.style.display = "block";
+    cabecalho.remove();
+    return true;
+  }
+}
+
+function myFunction() {
+  if (removerCabecalho() == false) {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
   }
 }
--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,20 @@
+function removerCabecalho() {
+  var cabecalho = document.querySelector("#cabecalho");
+  if (cabecalho == null) {
+    return false;
+  } else {
+    cabecalho.remove();
+    return true;
+  }
+}
+
 function myFunction() {
+  if (removerCabecalho() == false) {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
-        x.style.display = "none";
+      x.style.display = "none";
     } else {
-        x.style.display = "block";
+      x.style.display = "block";
     }
+  }
 }
